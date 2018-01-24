d37e7e49365c91f314f3fbf06c23d10cb78be370
Late Worm <travis.rempel@gmail.com>
2018-01-23 21:41:12 -0800

    Dispatch data about the the user to the store.

--- client/src/redux/modules/profile.js
+++ client/src/redux/modules/profile.js
@@ -1,14 +1,16 @@
 // Actions
 
-const GET_ITEMS_LOADING = 'GET_ITEMS_LOADING';
-const GET_ITEMS = 'GET_ITEMS';
-const GET_ITEMS_ERROR = 'GET_ITEMS_ERROR';
+const GET_ITEMS_LOADING = "GET_ITEMS_LOADING"; // TODO: figure out what this does
+const GET_ITEMS = "GET_ITEMS";
+const GET_USERS = "GET_USERS";
+const GET_ITEMS_ERROR = "GET_ITEMS_ERROR";
 
 // Action creators
 
 const getItemsLoading = () => ({ type: GET_ITEMS_LOADING });
-const getItems = (items) => ({ type: GET_ITEMS, payload: items });
-const getItemsError = (error) => ({ type: GET_ITEMS_ERROR, payload: error });
+const getItems = items => ({ type: GET_ITEMS, payload: items });
+const getUsers = users => ({ type: GET_USERS, payload: users });
+const getItemsError = error => ({ type: GET_ITEMS_ERROR, payload: error });
 
 // Async action creator
 
@@ -16,59 +18,75 @@ const ITEMS_URL = "http://localhost:4000/items";
 const USERS_URL = "http://localhost:4000/users";
 
 export const fetchItemsAndUser = userid => dispatch => {
-	dispatch(getItemsLoading());
+  // TODO: learn how this syntax works, and thus what this line does
+  dispatch(getItemsLoading()); // dispatch a Redux action of type GET_ITEMS_LOADING... see reducers at bottom of file
 
-	return Promise.all(
-		[`${ITEMS_URL}/?itemowner=${userid}`, USERS_URL] // query to retreive only items from the current user
-			.map(url => fetch(url)
-				.then(response => response.json())))
-		.then(response => {
-			const [itemsList, usersList] = response;
+  return Promise.all(
+    [`${ITEMS_URL}/?itemowner=${userid}`, USERS_URL] // query to retreive only items from the current user
+      .map(url => fetch(url).then(response => response.json()))
+  )
+    .then(response => {
+      const [itemsList, usersList] = response;
 
-			const allItemsFromUser = itemsList.map(item => {
-				// replace the itemowner hash with useful info about the owner
-				item.itemowner = usersList.find(user => user.id === item.itemowner);
-				if (item.borrower) {
-					// replace the borrower hash with useful info about the borrower
-					item.borrower = usersList.find(user => user.id === item.borrower);
-				}
-				return item;
-			});
+      const allItemsFromUser = itemsList.map(item => {
+        // replace the itemowner hash with useful info about the owner
+        item.itemowner = usersList.find(user => user.id === item.itemowner);
+        if (item.borrower) {
+          // replace the borrower hash with useful info about the borrower
+          item.borrower = usersList.find(user => user.id === item.borrower);
+        }
+        return item;
+      });
 
+      const allInfoAboutUser = usersList.find(user => user.id === userid);
 
+      dispatch(getItems(allItemsFromUser)); // send the list of items shared by this user to the store
+      dispatch(getUsers(allInfoAboutUser));
 
-			// TODO: map/filter to pass out just the one user we need
-
-			dispatch(getItems(allItemsFromUser));
-
-		}).catch(error => dispatch(getItemsError(error.message)));
-
+      // TODO: map/filter to pass out just the one user we need
+    })
+    .catch(error => dispatch(getItemsError(error.message)));
 };
 
 // Reducer
 
-export default (state = {
-	isLoading: false,
-	userItems: [],
-	error: ''
+export default (
+  state = {
+    isLoading: false,
+    userInfo: [],
+    userItems: [],
+    error: ""
+  },
+  action
+) => {
+  switch (action.type) {
+    case GET_ITEMS_LOADING: {
+      return { ...state, isLoading: true, error: "" };
+    }
 
-}, action) => {
+    case GET_ITEMS: {
+      return {
+        ...state,
+        isLoading: false,
+        error: "",
+        userItems: action.payload
+      }; // place data into state.userItems.userItems TODO: make the path less stupid
+    }
 
-	switch (action.type) {
+    case GET_USERS: {
+      return {
+        ...state,
+        isLoading: false,
+        error: "",
+        userInfo: action.payload
+      }; // place data into state.userItems.users TODO: make the path less stupid
+    }
 
-		case GET_ITEMS_LOADING: {
-			return { ...state, isLoading: true, error: '' };
-		}
+    case GET_ITEMS_ERROR: {
+      return { ...state, isLoading: false, error: action.payload };
+    }
 
-		case GET_ITEMS: {
-			return { ...state, isLoading: false, userItems: action.payload, error: '' } // creates state.users.userItems
-		}
-
-		case GET_ITEMS_ERROR: {
-			return { ...state, isLoading: false, error: action.payload }
-		}
-
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
