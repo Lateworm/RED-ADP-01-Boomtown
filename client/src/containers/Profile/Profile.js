ede67c67b2d8ccd9aabf5069ae9aa84c00a3b47e
Late Worm <travis.rempel@gmail.com>
2018-02-03 15:57:40 -0800

    Modify GraphQL query for Profile page.

--- client/src/containers/Profile/Profile.js
+++ client/src/containers/Profile/Profile.js
@@ -9,100 +9,101 @@ import CircularProgress from "material-ui/CircularProgress";
 
 // import from Material UI
 import {
-  Card,
-  CardActions,
-  CardHeader,
-  CardMedia,
-  CardTitle,
-  CardText
+	Card,
+	CardActions,
+	CardHeader,
+	CardMedia,
+	CardTitle,
+	CardText
 } from "material-ui/Card";
 import RaisedButton from "material-ui/RaisedButton";
 import Paper from "material-ui/Paper";
 
 const Profile = ({ list, userid }) => {
-  // list = array of all items to display
-  // userInfo = object of data about the user being profiled
+	// list = array of all items to display
+	// userInfo = object of data about the user being profiled
 
-  const masonryOptions = {
-    originTop: true
-  };
+	const masonryOptions = {
+		originTop: true
+	};
 
-  console.log("Logging Profile.js > const Profile > prop list ...");
-  console.log(list);
-  return (
-    <div>
-      <Paper
-        className="profile-paper"
-        zDepth={1}
-        children={
-          <div className="profile-div">
-            <div>
-              <h1>User Name</h1>
-              <h2>User Bio</h2>
-            </div>
-            <div>
-              <h3>Items shared</h3>
-              <h3>X Items borrowed</h3>
-              <Gravatar size={180} className="photo" email="User Email" />
-            </div>
-          </div>
-        }
-      />
+	console.log("Logging prop 'list': ", list);
+	console.log("Logging prop 'userid': ", userid);
 
-      <Masonry options={masonryOptions}>
-        {list.map(item => (
-          <Card className="card" key={item.id}>
-            {item.borrower ? (
-              // if the item is lent out to someone print their name on the overlay
-              <CardMedia
-                className="card-media"
-                overlay={
-                  <CardTitle subtitle={`lent to ${item.borrower.fullname}`} />
-                }
-              >
-                <img src={item.imageurl} alt={item.title} />
-              </CardMedia>
-            ) : (
-              // else don't generate an overlay at all
-              <CardMedia className="card-media">
-                <img src={item.imageurl} alt={item.title} />
-              </CardMedia>
-            )}
-            <a href={"/profile/" + item.itemowner.id}>
-              <CardHeader
-                title={item.itemowner.fullname}
-                subtitle={Moment(item.created).fromNow()}
-                avatar={
-                  <Gravatar className="photo" email={item.itemowner.email} />
-                }
-              />
-            </a>
-            <CardTitle title={item.title} subtitle={item.tags[0].title} />
-            <CardText>{item.description}</CardText>
-            {item.borrower ? (
-              // if the item is lent out to someone, don't render a Borrow button
-              ""
-            ) : (
-              // if the item is not lent out, render a borrow button
-              <CardActions>
-                <RaisedButton
-                  backgroundColor="#263238"
-                  labelColor="#fff"
-                  label="Borrow"
-                  className="borrow-button"
-                />
-              </CardActions>
-            )}
-          </Card>
-        ))}
-      </Masonry>
-    </div>
-  );
+	return (
+		<div>
+			<Paper
+				className="profile-paper"
+				zDepth={1}
+				children={
+					<div className="profile-div">
+						<div>
+							<h1>User Name</h1>
+							<h2>User Bio</h2>
+						</div>
+						<div>
+							<h3>Items shared</h3>
+							<h3>X Items borrowed</h3>
+							<Gravatar size={180} className="photo" email="User Email" />
+						</div>
+					</div>
+				}
+			/>
+
+			<Masonry options={masonryOptions}>
+				{list.map(item => (
+					<Card className="card" key={item.id}>
+						{item.borrower ? (
+							// if the item is lent out to someone print their name on the overlay
+							<CardMedia
+								className="card-media"
+								overlay={
+									<CardTitle subtitle={`lent to ${item.borrower.fullname}`} />
+								}
+							>
+								<img src={item.imageurl} alt={item.title} />
+							</CardMedia>
+						) : (
+							// else don't generate an overlay at all
+							<CardMedia className="card-media">
+								<img src={item.imageurl} alt={item.title} />
+							</CardMedia>
+						)}
+						<a href={"/profile/" + item.itemowner.id}>
+							<CardHeader
+								title={item.itemowner.fullname}
+								subtitle={Moment(item.created).fromNow()}
+								avatar={
+									<Gravatar className="photo" email={item.itemowner.email} />
+								}
+							/>
+						</a>
+						<CardTitle title={item.title} subtitle={item.tags[0].title} />
+						<CardText>{item.description}</CardText>
+						{item.borrower ? (
+							// if the item is lent out to someone, don't render a Borrow button
+							""
+						) : (
+							// if the item is not lent out, render a borrow button
+							<CardActions>
+								<RaisedButton
+									backgroundColor="#263238"
+									labelColor="#fff"
+									label="Borrow"
+									className="borrow-button"
+								/>
+							</CardActions>
+						)}
+					</Card>
+				))}
+			</Masonry>
+		</div>
+	);
 };
 
 Profile.prototype = {
-  list: PropTypes.array.isRequired,
-  userid: PropTypes.string.isRequired
+	list: PropTypes.array.isRequired,
+	userid: PropTypes.string.isRequired
 };
 
 export default Profile;
--- client/src/containers/Profile/ProfileContainer.js
+++ client/src/containers/Profile/ProfileContainer.js
@@ -5,45 +5,49 @@ import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 
 class ProfileContainer extends Component {
-  render() {
-    const { loading, items } = this.props.data;
-    return loading ? (
-      <p>Loading...</p>
-    ) : (
-      <Profile list={items} userid={this.props.match.params.userid} />
-    );
-  }
+	render() {
+		const { loading, user } = this.props.data;
+		return loading ? (
+			<p>Loading...</p>
+		) : (
+			<Profile list={user.shareditems} userid={user} />
+		);
+	}
 
-  // static propTypes = {
-  // isLoading: PropTypes.bool.isRequired, // TODO: make sure isLoading is always defined
-  // itemsData: PropTypes.array.isRequired, // An array of object, each item is an object
-  // error: PropTypes.string.isRequired
-  // };
+	// static propTypes = {
+	// isLoading: PropTypes.bool.isRequired, // TODO: make sure isLoading is always defined
+	// itemsData: PropTypes.array.isRequired, // An array of object, each item is an object
+	// error: PropTypes.string.isRequired
+	// };
 } // end class ProfileContainer
 
 const fetchItems = gql`
-  query {
-    items {
-      available
-      borrower {
-        id
-      }
-      description
-      id
-      imageurl
-      itemowner {
-        bio
-        email
-        fullname
-        id
-      }
-      tags {
-        id
-        title
-      }
-      title
-    }
-  }
+	query getUser($id: ID) {
+		user(id: $id) {
+			fullname
+			bio
+			shareditems {
+				available
+				borrower {
+					id
+				}
+				description
+				id
+				imageurl
+				itemowner {
+					bio
+					email
+					fullname
+					id
+				}
+				tags {
+					id
+					title
+				}
+				title
+			}
+		}
+	}
 `; // this becomes the prop 'list' in Profile.js
 
 // The old way - refer to this to help get user info back
@@ -51,4 +55,6 @@ const fetchItems = gql`
 //   userInfo: state.userItems.userInfo
 // });
 
-export default graphql(fetchItems)(ProfileContainer); // TODO: modify fetchItems to also fetch user info
+export default graphql(fetchItems, {
+	options: ({ match }) => ({ variables: { id: match.params.userid } })
+})(ProfileContainer); // TODO: modify fetchItems to also fetch user info
