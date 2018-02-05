1c3c12e060d44d64dfc49026aa8c9737e1cb4fb8
Late Worm <travis.rempel@gmail.com>
2018-02-04 20:06:25 -0800

    Add mutation for new items.

--- client/src/containers/Share/ShareContainer.js
+++ client/src/containers/Share/ShareContainer.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
 import Share from "./Share";
+
+import { graphql } from "react-apollo";
+import { compose } from "react-apollo"; // https://www.apollographql.com/docs/react/basics/setup.html#compose
+import gql from "graphql-tag";
+import { connect } from "react-redux";
 
 class ShareContainer extends Component {
 	render() {
 		return <Share share={this.share} />;
 	}
 }
+
+const createItem = gql`
+	mutation addItem(
+		$imageurl: String
+		$title: String
+		$description: String
+		$tags: [TagInput]
+	) {
+		addItem(
+			newItem: {
+				imageurl: $imageurl
+				title: $title
+				description: $description
+				tags: $tags
+			}
+		) {
+			imageurl
+			title
+			description
+			tags {
+				id
+				title
+			}
+		}
+	}
+`;
 
 export default ShareContainer;
